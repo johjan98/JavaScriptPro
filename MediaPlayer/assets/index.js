df0ac53c5f290cf2fc2bde3f64273f226edbf1b2
johjanpuerta <johjan_castano98@hotmail.com>
2022-03-05 18:57:37 -0500

    Clase #23 Service Worker (Adding sw.js)

--- MediaPlayer/assets/index.js
+++ MediaPlayer/assets/index.js
@@ -9,3 +9,7 @@ const muteButton = document.querySelector(".muteButton");
 const player = new MediaPlayer({el: video, plugins:[new AutoPlay(), new AutoPause()]});
 playButton.onclick = () => player.playPause();
 muteButton.onclick = () => player.mute();
+
+if ('serviceWorker' in navigator){
+  navigator.serviceWorker.register('./sw.js').catch(error => console.log(error.message));
+}
---
+++ MediaPlayer/sw.js
@@ -0,0 +1,48 @@
+
+const VERSION = "v1";
+
+//Se cargan los archivos dentro del caché cuando se abre por primera vez
+self.addEventListener('install', event =>{
+  event.waitUntil(precache());
+});
+
+self.addEventListener('fetch', event =>{
+  const request = event.request;
+  //Asegurar que se hace una petición GET
+  if(request.method !== "GET"){
+    return;
+  }
+
+  //Buscar en el caché si esta petición está disponible.
+  event.respondWith(cachedResponse(request));
+
+  //Actualizar caché. Si el user tiene conexión igual se va a buscar primero en el caché y va a retornar lo que haya ahí. Debemos verificar si hay cambios en los archivos y actualizar el caché para el que user siempre tenga la última versión de la app.
+  event.waitUntil(updateCache(request));
+});
+
+async function precache(){
+  const cache = await caches.open(VERSION);
+  cache.addAll([
+    '/',
+    '/index.html',
+    '/assets/index.js',
+    '/assets/MediaPlayer.js',
+    '/assets/plugins/AutoPlay.js',
+    '/assets/plugins/AutoPause.js',
+    '/assets/index.css',
+    '/assets/BigBuckBunny.mp4',
+  ]);
+}
+
+async function cachedResponse(request){
+  const cache = await caches.open(VERSION);
+  const response = await cache.match(request);
+  return response || fetch(request);   //Si no hay nada en el caché, se hace la petición online.
+}
+
+async function updateCache(request){
+  const cache = await caches.open(VERSION);
+  const response = await fetch(request);
+
+  return cache.put(request, response);
+}
