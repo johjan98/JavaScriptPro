c52c9ec48dbe779010759d159533250ee183e71e
johjanpuerta <johjan_castano98@hotmail.com>
2022-03-05 17:44:38 -0500

    Clase #22: document.addEventListener

--- MediaPlayer/assets/plugins/AutoPause.js
+++ MediaPlayer/assets/plugins/AutoPause.js
@@ -3,6 +3,7 @@ class AutoPause{
   constructor(){
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);   //De esta forma el this se refiere a la instancia de la clase cuando se llame la función desde otra instancia. Recordar que this siempre hace referencia a la instancia que llama una función, con bind se asegura que this se refiera sólo a la clase AutoPause.
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
 
   run(player){
@@ -10,12 +11,17 @@ class AutoPause{
     const observer = new IntersectionObserver(this.handleIntersection, {threshold: this.threshold});
 
     observer.observe(this.player.media);
+    document.addEventListener("visibilitychange", this.handleVisibilityChange)
   }
 
   handleIntersection(entries){
     const entry = entries[0];
 
-    entry.intersectionRatio >= this.threshold ? this.player.play() :this.player.pause();
+    entry.intersectionRatio >= this.threshold ? this.player.play() : this.player.pause();
+  }
+
+  handleVisibilityChange(){
+    document.visibilityState === "visible" ? this.player.play() : this.player.pause();
   }
 }
 
