b8cf2e13dbb1ee7bb56b4858e2d0313492aaef8b
johjanpuerta <johjan_castano98@hotmail.com>
2022-03-08 20:35:18 -0500

    Clase Tipos en Typescript

---
+++ MediaPlayer/ejercicios/typescript/index.ts
@@ -0,0 +1,14 @@
+
+//Boolean
+let muted: boolean = true;
+muted = false;
+
+//Números
+let numerador: number = 42;
+let denominador: number = 6;
+let resultado = numerador/denominador;
+
+//String
+let nombre: string = 'Johjan';
+let apellido: string = 'Puerta';
+let saludo = `Hola, soy ${nombre} ${apellido}`;
