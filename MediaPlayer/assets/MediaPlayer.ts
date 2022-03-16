f1818fe87a35df287743a24b6aceabf497e1e7f9
johjanpuerta <johjan_castano98@hotmail.com>
2022-03-16 14:31:17 -0500

    Final del curso

--- MediaPlayer/assets/MediaPlayer.ts
+++ MediaPlayer/assets/MediaPlayer.ts
@@ -4,24 +4,37 @@ class MediaPlayer {
 
   media: HTMLMediaElement;
   plugins: Array<any>;
+  container: HTMLElement;
 
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || []; //Con || [] se da un valor inicial si plugins viene vacío.
 
+    this.initPlayer();
     this.initPlugins();
   }
+
+  initPlayer(){
+    this.container = document.createElement('div');
+    this.container.style.position = 'relative';
+    this.media.parentNode.insertBefore(this.container, this.media);
+    this.container.appendChild(this.media);
+  }
+
   private initPlugins() {
     this.plugins.forEach(plugin => {
       plugin.run(this);
     });
   }
+
   play() {
     this.media.play();
   }
+
   pause() {
     this.media.pause();
   }
+
   playPause() {
     if (this.media.paused) {
       this.media.play();
@@ -29,6 +42,7 @@ class MediaPlayer {
       this.media.pause();
     }
   }
+
   mute() {
     if (this.media.muted) {
       this.media.muted = false;
--- MediaPlayer/assets/index.ts
+++ MediaPlayer/assets/index.ts
@@ -1,12 +1,13 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 const video = document.querySelector("video");
 const playButton: HTMLElement = document.querySelector(".playButton");
 const muteButton: HTMLElement = document.querySelector(".muteButton");
 
-const player = new MediaPlayer({el: video, plugins:[new AutoPlay(), new AutoPause()]});
+const player = new MediaPlayer({el: video, plugins:[new AutoPlay(), new AutoPause(), new Ads()]});
 playButton.onclick = () => player.playPause();
 muteButton.onclick = () => player.mute();
 
